5374c9d1101e661611d8cb2901dc557a422010fd
Joshua Commerford <joshua.commerford@gmail.com>
2024-07-06 20:49:00 +0200

    updates

--- staticfiles/recipes/script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-document.addEventListener('DOMContentLoaded', function () {
-    const messages = JSON.parse(document.getElementById('django-messages').textContent);
-    messages.forEach(message => {
-        alert(message.message);
-    });
-});
